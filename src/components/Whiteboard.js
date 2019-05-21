2f1382286043ba3c5d00b8fffa49a225b69db5ff
wnykuang <kuangwinnie@gmail.com>
2019-05-21 15:17:04 -0400

    update bootstrap

--- src/components/Whiteboard.js
+++ src/components/Whiteboard.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default class Whiteboard extends React.Component{
     render(){
         return (
-            <h1>White board!</h1>
+            <div className="container">
+                <h1>White board!</h1>
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css'
 import Whiteboard from './components/Whiteboard'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Whiteboard />,
+    document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
