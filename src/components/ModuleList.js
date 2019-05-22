cfb21c7ac7762e8a66715abb7d766be56ad2a098
wnykuang <kuangwinnie@gmail.com>
2019-05-21 21:46:08 -0400

    renderly from state

--- src/components/ModuleList.js
+++ src/components/ModuleList.js
@@ -2,18 +2,39 @@ import React from 'react'
 import ModuleItem from "./ModuleItem";
 
 export default class ModuleList extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            modules:
+                [
+                {id : 123, title : "Organic Chem"},
+                {id : 234, title : "Econ 101"},
+                {id : 456, title : "Quantum Phys"}
+            ]//different module for same course.
+        }; //has to be called state.
+    }
     render() {
         return (
             <div>
                 <h3>
                     Module List
                 </h3>
-                <ul className="list-group">
-                    <ModuleItem title="HTML"/>
-                    <ModuleItem title="JavaScript"/>
-                    <ModuleItem title="React"/>
-                    <ModuleItem title="BootStrap"/>
-                </ul>
+                {
+                    this.state.modules.map(
+                        // function (module) {
+                        //     return (
+                        //         <ModuleItem key={module.id} title={module.title}/>
+                        //     )
+                        module => <ModuleItem key={module.id} title={module.title}/>
+
+                    )
+                }
+                {/*<ul className="list-group">*/}
+                {/*    <ModuleItem title="HTML"/>*/}
+                {/*    <ModuleItem title="JavaScript"/>*/}
+                {/*    <ModuleItem title="React"/>*/}
+                {/*    <ModuleItem title="BootStrap"/>*/}
+                {/*</ul>*/}
             </div>
         )
     }
