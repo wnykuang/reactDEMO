a0b81370d17f57e3c1fdbff75e34f8faea2756bd
wnykuang <kuangwinnie@gmail.com>
2019-05-22 08:06:37 -0400

    update module list

--- src/components/ModuleList.js
+++ src/components/ModuleList.js
@@ -2,33 +2,46 @@ import React from 'react'
 import ModuleItem from "./ModuleItem";
 
 export default class ModuleList extends React.Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
+            module: {
+                id: -1,
+                title: ''
+            },
             modules:
                 [
-                {id : 123, title : "Organic Chem"},
-                {id : 234, title : "Econ 101"},
-                {id : 456, title : "Quantum Phys"}
-            ]//different module for same course.
+                    {id: 123, title: "Organic Chem"},
+                    {id: 234, title: "Econ 101"},
+                    {id: 456, title: "Quantum Phys"}
+                ]//different module for same course.
         }; //has to be called state.
     }
+
     render() {
         return (
             <div>
                 <h3>
                     Module List
                 </h3>
-                {
-                    this.state.modules.map(
-                        // function (module) {
-                        //     return (
-                        //         <ModuleItem key={module.id} title={module.title}/>
-                        //     )
-                        module => <ModuleItem key={module.id} title={module.title}/>
-
-                    )
-                }
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <input
+                            value={this.state.module.title}
+                            className="form-control"/>
+                        <button className="btn btn-primary btn-block"> Add Module
+                        </button>
+                    </li>
+                    {
+                        this.state.modules.map(
+                            // function (module) {
+                            //     return (
+                            //         <ModuleItem key={module.id} title={module.title}/>
+                            //     )
+                            module => <ModuleItem key={module.id} title={module.title}/>
+                        )
+                    }
+                </ul>
                 {/*<ul className="list-group">*/}
                 {/*    <ModuleItem title="HTML"/>*/}
                 {/*    <ModuleItem title="JavaScript"/>*/}
